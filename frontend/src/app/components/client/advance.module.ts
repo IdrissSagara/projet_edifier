c33eea6b9c81976aa84d1b80ae56429764b9c910
Drissa Cissé <comptes.kh@protonmail.com>
2018-03-02 17:21:49 +0000

    [f] ajout du dialog client mais affichage pas Ok

--- frontend/src/app/components/client/advance.module.ts
+++ frontend/src/app/components/client/advance.module.ts
@@ -5,14 +5,15 @@ import {RouterModule} from '@angular/router';
 import {AdvanceRoutes} from './advance.routing';
 import {ClientComponent} from './client.component';
 import {SharedModule} from '../../shared/shared.module';
+import {ClientDialogComponent} from './client-dialog/client-dialog.component';
+import {DialogModule} from 'primeng/primeng';
+import {NgSelectModule} from '@ng-select/ng-select';
 
 @NgModule({
-  imports: [
-    CommonModule,
+  imports: [CommonModule, DialogModule, NgSelectModule,
     RouterModule.forChild(AdvanceRoutes),
     SharedModule
-  ],
-  declarations: [AdvanceComponent, ClientComponent
+  ], declarations: [AdvanceComponent, ClientComponent, ClientDialogComponent
   ]
 })
 export class AdvanceModule { }
--- frontend/src/app/components/client/client.component.ts
+++ frontend/src/app/components/client/client.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ToastData, ToastOptions, ToastyService} from 'ng2-toasty';
+import {Client} from '../../models/client';
 
 @Component({
   selector: 'app-client', templateUrl: './client.component.html'
@@ -13,9 +14,29 @@ export class ClientComponent implements OnInit {
   theme = 'bootstrap';
   type = 'default';
   closeOther = false;
-  constructor(private toastyService: ToastyService) {}
+
+  showDialog = false;
+  clientToAdd: Client = new Client();
+
+  constructor(private toastyService: ToastyService) {
+  }
 
   ngOnInit() {
+  }
+
+  initDataTable() {
+    // Initialiser ici le contenu de la table des clients
+  }
+
+  onHide(): void {
+    this.showDialog = false;
+    this.initDataTable();
+  }
+
+  showClientDialog() {
+    console.log('Bien reçu commandant!!');
+    this.showDialog = true;
+    this.clientToAdd = new Client();
   }
 
   addToast(options) {
@@ -38,12 +59,24 @@ export class ClientComponent implements OnInit {
     };
 
     switch (options.type) {
-      case 'default': this.toastyService.default(toastOptions); break;
-      case 'info': this.toastyService.info(toastOptions); break;
-      case 'success': this.toastyService.success(toastOptions); break;
-      case 'wait': this.toastyService.wait(toastOptions); break;
-      case 'error': this.toastyService.error(toastOptions); break;
-      case 'warning': this.toastyService.warning(toastOptions); break;
+      case 'default':
+        this.toastyService.default(toastOptions);
+        break;
+      case 'info':
+        this.toastyService.info(toastOptions);
+        break;
+      case 'success':
+        this.toastyService.success(toastOptions);
+        break;
+      case 'wait':
+        this.toastyService.wait(toastOptions);
+        break;
+      case 'error':
+        this.toastyService.error(toastOptions);
+        break;
+      case 'warning':
+        this.toastyService.warning(toastOptions);
+        break;
     }
   }
 }
