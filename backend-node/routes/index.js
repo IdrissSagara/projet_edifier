3fd455a53326c0f15df2ec34f27f8c95850ba752
cashenvic <comptes.kh@protonmail.com>
2020-02-25 13:15:11 +0100

    [b] added a presentation site accessible on backend root uri

--- backend-node/routes/index.js
+++ backend-node/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const config = require('../config/jwt_config');
+const content = require('../config/content');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: config.app_name, basic: content.basic, pro: content.pro, enterprise: content.enterprise});
 });
 
 module.exports = router;
