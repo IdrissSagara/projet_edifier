4e70de281e721bddf742e22e2db47a2e5da8ff6e
cashenvic <comptes.kh@protonmail.com>
2020-03-22 19:42:01 +0100

    [f] added empty table model to chantier

--- client/src/app/chantier/chantier.component.ts
+++ client/src/app/chantier/chantier.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ChantierService} from "../services/chantier.service";
 
 @Component({
   selector: 'app-chantier',
@@ -6,11 +7,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./chantier.component.css']
 })
 export class ChantierComponent implements OnInit {
+  chantiers;
 
-  constructor() {
+  constructor(private chantierService: ChantierService) {
   }
 
   ngOnInit(): void {
+    this.getAllChantiers();
   }
 
+  getAllChantiers() {
+    this.chantierService.getAllChantier().then(res => {
+      this.chantiers = res.rows;
+    }).catch(err => {
+      console.log("error during getting all the chantiers");
+      console.log(err);
+    });
+  }
 }
