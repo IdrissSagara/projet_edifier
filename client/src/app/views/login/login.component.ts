905dd8b4e0abc55b78b1a438ffea13ca051bfb48
Sagara Idriss <sagara.idriss@gmail.com>
2020-03-22 18:32:10 +0100

    authentication in place

--- client/src/app/views/login/login.component.ts
+++ client/src/app/views/login/login.component.ts
@@ -1,8 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  username;
+  password;
+  errorMessage;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    if (this.authService.isloggedIn()) {
+      this.router.navigate(['']);
+    }
+  }
+
+  login() {
+    this.errorMessage = '';
+    this.authService.login(this.username, this.password).then(r => {
+      console.log("this is the result of login");
+      console.log(r);
+      // if the returned body is not empty then redirect otherwise do nothing
+      this.router.navigate(['dashboard']);
+    }).catch(err => {
+      console.log('oops during login');
+      console.log(err);
+      this.errorMessage = err.error.error;
+    });
+  }
 }
