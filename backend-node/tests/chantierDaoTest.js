4dc4d3c1d98362b8b9c534952a6848b2443b74d4
Sagara Idriss <sagara.idriss@gmail.com>
2020-04-08 16:31:35 +0200

    [b] update chantier dao and test test accordingly

--- backend-node/tests/chantierDaoTest.js
+++ backend-node/tests/chantierDaoTest.js
@@ -33,3 +33,35 @@ describe("getChantierById", () => {
         assert(result === null, 'Le chantier doit etre null');
     });
 });
+
+describe("update chantier", () => {
+    let chantier = {
+        id: 3,
+        emplacement: "boul city",
+        cout: 323,
+        date_debut: "2019-12-31T23:00:00.000Z",
+        date_fin: null,
+        walita: 0,
+        yereta: 323000,
+        montant_dispo: 323000,
+        createdAt: "2019-12-30T02:08:04.000Z",
+        updatedAt: "2020-03-01T04:57:33.000Z",
+        ClientId: 2,
+    };
+
+    it("Should update the chantier with the id 3", async () => {
+        const result = await chantierDAO.update(chantier);
+        //console.log(result);
+        assert.isDefined(result);
+        assert.notEqual(result.status, 'error', 'must success');
+        assert(result[0] === 1, 'must return 1')
+    });
+
+    it("Should fail trying to update inextant chantier", async () => {
+        chantier.id = 99999;
+        const result = await chantierDAO.update(chantier);
+        console.log(result);
+        assert.isDefined(result);
+        assert(result[0] === 0, 'must return O')
+    });
+});
