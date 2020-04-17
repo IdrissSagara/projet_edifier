46a58339a3d4ac8d74010ae7cee940ce80fe2e46
cashenvic <comptes.kh@protonmail.com>
2020-04-17 22:57:39 +0200

    [f] redirect user to his url after login

--- client/src/app/authentication/authGuard/auth.guard.ts
+++ client/src/app/authentication/authGuard/auth.guard.ts
@@ -18,12 +18,12 @@ export class AuthGuard implements CanActivate {
       if (!route.data.roles || route.data.roles.indexOf(this.auth.getRole()) !== -1) {
         return true;
       } else { // user is auth but does not have accreditation to see this route
-        this.router.navigate(['/login']);
+        this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
         console.log('Vous n\'avez pas les droits nécessaires pour acceder à cette route');
         return false;
       }
     } else {
-      this.router.navigate(['/login']).then(r => {
+      this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}}).then(r => {
         console.error("Vous devez vous authentifier pour acceder à cette ressource");
         // this.toastService.toastError("Vous devez vous authentifier pour acceder à cette ressource");
       });
