65ad8df4d8e38fec30255689ff6068657c5b2058
cashenvic <comptes.kh@protonmail.com>
2020-04-19 15:48:02 +0200

    [f] refactor paiement service(proper angular httpClient)

--- client/src/app/services/paiement.service.ts
+++ client/src/app/services/paiement.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../environments/environment";
 import {AllPaiementResponse} from "../model/responses/AllPaiementResponse";
 import {Observable} from "rxjs";
 import {HttpClient} from '@angular/common/http';
+import {Paiement} from "../model/paiement";
 
 @Injectable({
   providedIn: 'root'
@@ -14,53 +15,13 @@ export class PaiementService {
   constructor(private http: HttpClient) {
   }
 
-  async getAllPaiement(offset = 0): Promise<AllPaiementResponse> {
-    return new Promise<AllPaiementResponse>(((resolve, reject) => {
-      this.http.get(`${this.apiUrl}/all?offset=${offset}`, {responseType: 'text'}).toPromise().then(res => {
-          resolve(JSON.parse(res));
-        }, rej => {
-          reject(rej);
-        }
-      );
-    }));
+  getAllPaiement(offset = 0): Observable<AllPaiementResponse> {
+    return this.http.get<AllPaiementResponse>(`${this.apiUrl}/all?offset=${offset}`);
   }
 
-  async getPaimentById(id: number): Promise<any> {
-    return new Promise<any>(((resolve, reject) => {
-      this.http.get(`${this.apiUrl}/chantier/${id}`, {responseType: 'text'}).toPromise().then(
-        res => {
-          resolve(JSON.parse(res));
-        }, rej => {
-          reject(rej);
-        }
-      );
-    }));
+  getPaimentById(id: number) {
+    return this.http.get<Paiement>(`${this.apiUrl}/chantier/${id}`);
   }
-
-
-  /*       getPaimentFacture(idFacture: number): Promise<any> {
-          return new Promise<any>(((resolve, reject) => {
-            this.http.get(`${this.apiUrl}/${idFacture}/facture`, {responseType: "blob"}).toPromise().then(
-              res => {
-                return res;
-              }, rej => {
-                reject(rej);
-              }
-            );
-          }));
-        }*/
-
-  /*  private handleError(error: HttpErrorResponse) {
-      if (error.error instanceof ErrorEvent) {
-        console.error('An error occurred:', error.error.message);
-      } else {
-        console.error(
-          `Backend returned code ${error.status}, ` +
-          `body was: ${error.error}`);
-      }
-      // return an observable with a user-facing error message
-    }*/
-
 
   getPaimentFacture(idFacture: number): Observable<any> {
     return this.http.get(`${this.apiUrl}/${idFacture}/facture`, {responseType: "arraybuffer"});
