d923dd174ad0ce4b1855332467d5a2a4d7cac4af
Sagara Idriss <sagara.idriss@gmail.com>
2020-04-22 17:14:17 +0200

    [f] add post methode in ouvrier

--- client/src/app/services/ouvrier.service.ts
+++ client/src/app/services/ouvrier.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, throwError} from "rxjs";
 import {AllOuvrierResponse} from "../model/responses/AllOuvrierResponse";
 import {environment} from "../../environments/environment";
 import {Ouvrier} from "../model/ouvrier";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,24 @@ export class OuvrierService {
 
   updateOuvrier(ouvrier): Observable<Ouvrier> {
     return this.http.put<Ouvrier>(`${this.apiUrl}`, ouvrier);
+  }
+
+  addOuvrier(ouvrier: Ouvrier): Observable<Ouvrier> {
+    return this.http.post<Ouvrier>(`${this.apiUrl}`, ouvrier)
+      .pipe(catchError<any, any>(this.handleError));
+  }
+
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
   }
 
 }
