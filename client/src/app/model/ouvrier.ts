f8a4773004d098cbcaca268baf900739026074eb
Sagara Idriss <sagara.idriss@gmail.com>
2020-04-26 17:14:13 +0200

    [f] change model ouvrier

--- client/src/app/model/ouvrier.ts
+++ client/src/app/model/ouvrier.ts
@@ -8,6 +8,15 @@ export class Ouvrier {
   updatedAt: string;
   createdBy: number;
   updatedBy: number;
+
+  constructor() {
+    this.type = MACON;
+  }
 }
 
+export const MACON = 'Maçon';
+export const MENUISIER = 'Menuisier';
+export const CHEF_MACON = 'Chef maçon';
+export const ELECTRICIEN = 'Electricien';
 
+export const TYPES_OUVRIER = [MACON, MENUISIER, CHEF_MACON, ELECTRICIEN];
