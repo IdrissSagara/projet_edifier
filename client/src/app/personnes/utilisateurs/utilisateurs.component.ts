e28664bdedcfac8e425ac0c982a0adbf050a0afa
Sagara Idriss <sagara.idriss@gmail.com>
2020-05-03 18:35:12 +0200

    [f] creation de l'utilisateur

--- client/src/app/personnes/utilisateurs/utilisateurs.component.ts
+++ client/src/app/personnes/utilisateurs/utilisateurs.component.ts
@@ -1,9 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {UtilisateurService} from "../../services/utilisateur.service";
 import {Utilisateur} from "../../model/utilisateur";
-import {Subscription} from "rxjs";
+import {combineLatest, Subscription} from "rxjs";
 import {ToastrService} from "ngx-toastr";
 import {SpinnerService} from "../../services/spinner.service";
+import {BsModalRef, BsModalService} from "ngx-bootstrap";
+import {UtilisateurModalComponent} from "./utilisateur-modal/utilisateur-modal.component";
 
 @Component({
   selector: 'app-utilisateurs',
@@ -16,10 +18,12 @@ export class UtilisateursComponent implements OnInit {
   errorMessage: String;
   totalPages: number;
   subscriptions: Subscription[] = [];
+  utilisateurModalRef: BsModalRef;
   currentPage: number;
 
   constructor(private utilisateurService: UtilisateurService, private toastService: ToastrService,
-              private spinner: SpinnerService) {
+              private spinner: SpinnerService, private modalService: BsModalService,
+              private changeDetection: ChangeDetectorRef) {
   }
 
   get isLoading() {
@@ -42,5 +46,45 @@ export class UtilisateursComponent implements OnInit {
       });
     });
   }
+
+  unsubscribe() {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
+    this.subscriptions = [];
+  }
+
+  showAddUserDialog() {
+    const initialState = {
+      utilisateur: this.newUtilisateur = new Utilisateur(),
+      title: 'Ajouter un nouveau utilisateur'
+    };
+
+    const _combine = combineLatest(
+      this.modalService.onShown,
+      this.modalService.onHidden
+    ).subscribe(() => this.changeDetection.markForCheck());
+
+    this.subscriptions.push(
+      this.modalService.onShown.subscribe((reason: string) => {
+        // initialisa
+      })
+    );
+    this.subscriptions.push(
+      this.modalService.onHidden.subscribe((reason: string) => {
+        if (reason === null) {
+          this.getAllUtilisateur();
+        }
+
+        this.unsubscribe();
+      })
+    );
+
+    this.subscriptions.push(_combine);
+
+    this.utilisateurModalRef = this.modalService.show(UtilisateurModalComponent, {initialState});
+    this.utilisateurModalRef.content.closeBtnName = 'Close';
+  }
+
 
 }
