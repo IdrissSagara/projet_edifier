41b53fd9b63df9a4574356e4513ba8191dd33ccb
Sagara Idriss <sagara.idriss@gmail.com>
2020-06-16 01:09:05 +0200

    [f] add agence formulaire

--- client/src/app/parametrages/agence/agence.component.ts
+++ client/src/app/parametrages/agence/agence.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {NgModel} from "@angular/forms";
+import {Agence} from "../../model/agence";
 
 @Component({
   selector: 'app-agence',
@@ -6,6 +8,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./agence.component.css']
 })
 export class AgenceComponent implements OnInit {
+  showError: boolean = false;
+  agence: Agence;
+  erreursServeur: an
+  y;
 
   constructor() {
   }
@@ -13,4 +19,11 @@ export class AgenceComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  enregistrerFormulaire(logoInput: HTMLInputElement) {
+
+  }
+
+  inputEnErreur(input: NgModel) {
+
+  }
 }
--- client/src/app/parametrages/parametrage.module.ts
+++ client/src/app/parametrages/parametrage.module.ts
@@ -2,13 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AgenceComponent} from './agence/agence.component';
 import {RoutingModule} from "./parametrage.routing.module";
+import {AlertModule} from "ngx-bootstrap";
+import {FormsModule} from "@angular/forms";
+import {NgxSelectModule} from "ngx-select-ex";
 
 
 @NgModule({
   declarations: [AgenceComponent],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    AlertModule,
+    FormsModule,
+    NgxSelectModule
   ]
 })
 export class ParametrageModule {
