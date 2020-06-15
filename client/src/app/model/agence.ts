b860bd90356977ac92d55da9234f501ebfd7d771
Sagara Idriss <sagara.idriss@gmail.com>
2020-06-16 01:08:30 +0200

    [f] add agence model

---
+++ client/src/app/model/agence.ts
@@ -0,0 +1,14 @@
+export class Agence {
+  rccm: string;
+  fiscal: string;
+  libelle: string;
+  telephone: string;
+  fax: string;
+  mail: string;
+  adresse: string;
+  logo: string;
+  created_at: string;
+  updated_at: string;
+  created_by: string;
+  updated_by: string;
+}
