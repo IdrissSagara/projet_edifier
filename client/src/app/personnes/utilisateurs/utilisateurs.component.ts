6e0c21b2e04fe1d676f1b3927b49ffd7ca092d63
Sagara Idriss <sagara.idriss@gmail.com>
2020-07-15 15:17:42 +0200

    [f] pouvoir supprimer un utilisateur s'il est admin

--- client/src/app/personnes/utilisateurs/utilisateurs.component.ts
+++ client/src/app/personnes/utilisateurs/utilisateurs.component.ts
@@ -91,4 +91,8 @@ export class UtilisateursComponent implements OnInit {
   canEdit() {
     return [USER_ROLES.ADMIN, USER_ROLES.ADVANCEDUSER].includes(this.authService.getRole());
   }
+
+  canDelete() {
+    return [USER_ROLES.ADMIN].includes(this.authService.getRole());
+  }
 }
