9be9e7e3295303f6acf88feb510b44f8c6e911b5
cashenvic <comptes.kh@protonmail.com>
2020-07-22 20:44:25 +0200

    [f] Chantier: make use of store to get all items

--- client/src/app/chantier/chantier.component.ts
+++ client/src/app/chantier/chantier.component.ts
@@ -1,19 +1,22 @@
-import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ChantierService} from "../services/chantier.service";
 import {Chantier} from "../model/chantier";
 import {ChantierModalComponent} from "./chantier-modal/chantier-modal.component";
-import {combineLatest, Subscription} from "rxjs";
+import {combineLatest, Observable, Subscription} from "rxjs";
 import {BsModalRef, BsModalService, ModalDirective} from "ngx-bootstrap/modal";
 import {ToastrService} from "ngx-toastr";
 import {SpinnerService} from "../services/spinner.service";
-import {finalize, first} from "rxjs/operators";
+import {finalize, first, tap} from "rxjs/operators";
+import {Select, Store} from "@ngxs/store";
+import {ChantierState} from "../store/chantiers/chantier.state";
+import {GetChantiers} from "../store/chantiers/chantier.actions";
 
 @Component({
   selector: 'app-chantier',
   templateUrl: './chantier.component.html',
   styleUrls: ['./chantier.component.css']
 })
-export class ChantierComponent implements OnInit {
+export class ChantierComponent implements OnInit, OnDestroy {
   chantiers: Chantier[] = [];
   chantier: Chantier;
   chantierModalRef: BsModalRef;
@@ -27,31 +30,25 @@ export class ChantierComponent implements OnInit {
   currentPage: number;
 
   @ViewChild('dangerModal') public dangerModal: ModalDirective;
+  @Select(ChantierState.getChantiers) chantiers$: Observable<Chantier[]>;
+  @Select(ChantierState.areChantiersLoaded) areChantiersLoaded$;
+  areChantiersLoadedSub: Subscription;
 
   constructor(private chantierService: ChantierService,
               private modalService: BsModalService,
-              private changeDetection: ChangeDetectorRef,
+              private changeDetection: ChangeDetectorRef, private store: Store,
               private toastService: ToastrService, private spinner: SpinnerService) {
   }
 
   ngOnInit(): void {
-    this.getAllChantiers();
-  }
-
-  getAllChantiers(offset = 0) {
-    // this.isLoading = true;
-    this.spinner.show();
-    this.chantierService.getAllChantier(offset).pipe(first(), finalize(() => this.spinner.hide())).subscribe(res => {
-      this.errorMessage = undefined;
-      this.chantiers = res.rows;
-      this.totalItems = res.count;
-    }, err => {
-      this.errorMessage = "data loading error";
-      this.toastService.error('Une erreur est survenu lors de la récuperation des chantiers', '', {
-        progressBar: true,
-        closeButton: true,
-        tapToDismiss: false
-      });
+    // this.getAllChantiers();
+    this.areChantiersLoadedSub = this.areChantiersLoaded$.pipe(
+      tap((areChantiersLoaded) => {
+        if (!areChantiersLoaded) {
+          this.store.dispatch(new GetChantiers());
+        }
+      })
+    ).subscribe(value => {
     });
   }
 
@@ -74,7 +71,7 @@ export class ChantierComponent implements OnInit {
     this.subscriptions.push(
       this.modalService.onHidden.subscribe((reason: string) => {
         if (reason === null) {
-          this.getAllChantiers();
+          // this.getAllChantiers();
         }
 
         this.unsubscribe();
@@ -106,7 +103,7 @@ export class ChantierComponent implements OnInit {
     this.subscriptions.push(
       this.modalService.onHidden.subscribe((reason: string) => {
         if (reason === null) {
-          this.getAllChantiers();
+          // this.getAllChantiers();
         }
 
         this.unsubscribe();
@@ -139,7 +136,7 @@ export class ChantierComponent implements OnInit {
   confirmSupprimerChantier(): void {
     this.spinner.show();
     this.chantierService.deleteChantierById(this.delId).pipe(first(), finalize(() => this.spinner.hide())).subscribe(res => {
-      this.getAllChantiers();
+      // this.getAllChantiers();
       this.toastService.success('Chantier suppimer avec succes', '', {
         progressBar: true,
         closeButton: true,
@@ -167,6 +164,10 @@ export class ChantierComponent implements OnInit {
 
   pageChanged(event: any): void {
     const offset = (event.page - 1) * 10;
-    this.getAllChantiers(offset);
+    // this.getAllChantiers(offset);
+  }
+
+  ngOnDestroy() {
+    this.areChantiersLoadedSub.unsubscribe();
   }
 }
